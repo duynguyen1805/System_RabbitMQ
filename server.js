b2d4ab8aa3560a75570e9ceac43bbf9d691775c3
TDN <duynguyenqwert@gmail.com>
2024-06-25 18:25:14 +0700

    feat 🚀 : example RabbitMQ handle msg when MQserver err|die TTL using DLX (dead letter exchange)

--- server.js
+++ server.js
@@ -2,12 +2,30 @@
 
 const consumerMessageService = require("./src/services/consumerQueue.service");
 
-const queueName = "test-topic-queue";
+// const queueName = "test-topic-queue";
+// consumerMessageService
+//   .consumerToQueue(queueName)
+//   .then(() => {
+//     console.log("Message Consumer is running... with Queue name: ", queueName);
+//   })
+//   .catch((error) => {
+//     console.error("Message Consumer error: ", error);
+//   });
+
 consumerMessageService
-  .consumerToQueue(queueName)
+  .consumnerToQueueNormal()
   .then(() => {
-    console.log("Message Consumer is running... with Queue name: ", queueName);
+    console.log("Message Consumer Normal is running...");
   })
   .catch((error) => {
-    console.error("Message Consumer error: ", error);
+    console.error("Message Consumer Normal error: ", error);
+  });
+
+consumerMessageService
+  .consumnerToQueueFailed()
+  .then(() => {
+    console.log("Message Consumer Failed is running...");
+  })
+  .catch((error) => {
+    console.error("Message Consumer Failed error: ", error);
   });
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -14,6 +14,82 @@ const consumerMessageService = {
       );
     }
   },
+
+  // case processing
+  consumnerToQueueNormal: async () => {
+    try {
+      const { connection, channel } = await connectToRabbitMQ();
+      const notificationQueue = "notiQueue";
+
+      // CASE processing error ==> hot fix
+      //   setTimeout(() => {
+      //     channel.consume(notificationQueue, (message) => {
+      //       console.log(
+      //         "Send notificationQueue success with message: ",
+      //         message.content.toString()
+      //       );
+      //       channel.ack(message);
+      //     });
+      //   }, 15000);
+
+      channel.consume(notificationQueue, (message) => {
+        console.log(
+          "Send notificationQueue success with message: ",
+          message.content.toString()
+        );
+        channel.ack(message);
+      });
+    } catch (error) {
+      console.error(
+        "[consumerMessageService.consumnerToQueueNormal] Error when consumer message: ",
+        error
+      );
+    }
+  },
+
+  // case failed processing
+  consumnerToQueueFailed: async () => {
+    try {
+      const { connection, channel } = await connectToRabbitMQ();
+      const notificationExchangeDLX = "notiExchangeDLX";
+      const notificationRoutingKeyDLX = "notiRoutingKeyDLX";
+
+      const notificationQueueHotFix = "notiQueueHotFix"; // queue hotfix handler message error
+
+      await channel.assertExchange(notificationExchangeDLX, "direct", {
+        durable: true,
+      });
+
+      const queueHotfix = await channel.assertQueue(notificationQueueHotFix, {
+        exclusive: false,
+      });
+
+      await channel.bindQueue(
+        queueHotfix.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+
+      await channel.consume(
+        queueHotfix.queue,
+        (messageFailed) => {
+          console.log(
+            "This is notification error, pls hot fix:: ",
+            messageFailed.content.toString()
+          );
+        },
+        {
+          noAck: true,
+        }
+      );
+    } catch (error) {
+      console.error(
+        "[consumerMessageService.consumnerToQueueFailed] Error when consume messageFailed: ",
+        error
+      );
+      throw Error(error);
+    }
+  },
 };
 
 module.exports = consumerMessageService;
