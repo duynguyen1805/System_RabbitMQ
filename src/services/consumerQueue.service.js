48aaedb862379020b2465ad41e00d9749127c7c1
TDN <duynguyenqwert@gmail.com>
2024-06-26 11:47:17 +0700

    feat 🚀 : example RabbitMQ handle msg when ProcessLogicAPP error using DLX (dead letter exchange)

--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -2,6 +2,12 @@
 
 const { connectToRabbitMQ, consumerQueue } = require("../dbs/init.rabbit");
 
+// custom console.log
+const log = console.log;
+console.log = function () {
+  log.apply(console, [new Date()].concat(arguments));
+};
+
 const consumerMessageService = {
   consumerToQueue: async (queueName) => {
     try {
@@ -21,24 +27,42 @@ const consumerMessageService = {
       const { connection, channel } = await connectToRabbitMQ();
       const notificationQueue = "notiQueue";
 
-      // CASE processing error ==> hot fix
-      //   setTimeout(() => {
-      //     channel.consume(notificationQueue, (message) => {
-      //       console.log(
-      //         "Send notificationQueue success with message: ",
-      //         message.content.toString()
-      //       );
-      //       channel.ack(message);
-      //     });
-      //   }, 15000);
+      //CASE: 1 - MQ processing error TTL ==> hot fix
+      // const timeExpried = 0;
+      // setTimeout(() => {
+      //   channel.consume(notificationQueue, (message) => {
+      //     console.log(
+      //       "Send notificationQueue success with message: ",
+      //       message.content.toString()
+      //     );
+      //     channel.ack(message);
+      //   });
+      // }, timeExpried);
 
-      channel.consume(notificationQueue, (message) => {
-        console.log(
-          "Send notificationQueue success with message: ",
-          message.content.toString()
-        );
-        channel.ack(message);
-      });
+      /* CASE: 2 - Logic error ==> hot fix */
+      try {
+        channel.consume(notificationQueue, (message) => {
+          const numberTest = Math.random();
+          console.log("numberTest:: ", numberTest);
+          if (numberTest < 0.8) {
+            throw new Error("numberTest < 0.8");
+          }
+
+          console.log(
+            "SEND NOTIFICATION SUCCESS - Msg: ",
+            message.content.toString()
+          );
+          channel.ack(message);
+        });
+      } catch (error) {
+        // console.error("Send notification to QueueFailed ::: HOT FIX ", error);
+        channel.nack(message, false, false);
+        /*
+         nack : negative acknowledge
+         false: requeue? => có đẩy ngược lại queue lần nữa hay không, true => đẩy ngược lại, false => đẩy xuống queueFailed
+         false: từ chối tất cả hay 1, false => chỉ msg hiện tại bị từ chối
+        */
+      }
     } catch (error) {
       console.error(
         "[consumerMessageService.consumnerToQueueNormal] Error when consumer message: ",
